f2cf832bfae03d77abca322a53915ae98a0fa874
Zeuxis Lo <seekstudio@gmail.com>
2011-03-15 15:59:25 +0800

    Supported jkforum

--- content.js
+++ content.js
@@ -82,6 +82,33 @@ var tenkucity_com = function() {
 	}
 };
 
+var jkforum_net = function() {
+	debug('Init jkforum class');
+	
+	this.run = function() {
+		debug('Strat run');
+		
+		var $post_block = $("table.fo:first-child").parent();
+		var pid = $post_block.attr("id").replace("post_", "");
+		var tid = window.location.pathname.match(/thread-(\d+)-\d+-\d+\.html/)[1];
+		
+		debug("Block: " + $post_block);
+		debug("PID: " + pid);
+
+		ajax_it("http://www.jkforum.net/forum.php", "mod=viewthread&show=true&tid=" + tid + "&viewpid=" + pid + "&inajax=1", "xml").success(function(xml_doc) {
+			debug(xml_doc);
+			
+			var content = $("root", xml_doc).text();
+			
+			debug(content);
+			
+			$post_block.html(content);
+			
+			debug("Ended Ajax & Changed Content");
+		});
+	}
+};
+
 (function($) {
 	
 	debug("Start JetIt");

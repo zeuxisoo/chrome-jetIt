a6e4061a1d02b1feabc4c9702cc18dcc4354258a
Zeuxis Lo <seekstudio@gmail.com>
2011-03-15 13:29:58 +0800

    Added version number and modified date

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 /*
- * Author: Zeuxis Lo (http://zeuxis.me)
- * Create: 2011-03-14 14:07
+ * Author  : Zeuxis Lo (http://zeuxis.me)
+ * Version : 0.2
+ * Create  : 2011-03-14 14:07
+ * Modified: 2011-03-15 13:28
  */
 
 // Configure
